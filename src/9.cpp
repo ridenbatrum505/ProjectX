bf419d36933fb5f811d9a9704785dbbd9c9e2f18
ridenbatrum505 <ridenbatrum505@gmail.com>
2025-03-14 17:19:13 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name = "John";
+    int age = 25;
+    cout << "Hello, my name is " + name + " and I am " + to_string(age) + " years old." << endl;
+}
