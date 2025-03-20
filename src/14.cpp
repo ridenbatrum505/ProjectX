ecfe7b9bbc1a498cefb0452f83ea52be0993b86d
ridenbatrum505 <ridenbatrum505@gmail.com>
2025-03-20 17:09:39 +0000

    Add 14.cpp

---
+++ src/14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    if (x > 5) {
+        cout << "The value of x is greater than 5." << endl;
+    } else {
+        cout << "The value of x is less than or equal to 5." << endl;
+    }
+    return 0;
+}
