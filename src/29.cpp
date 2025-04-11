b9a4ce9cbc6e5543c55db797e28b58eba8287d16
ridenbatrum505 <ridenbatrum505@gmail.com>
2025-04-11 12:09:11 +0000

    Add 29.cpp

---
+++ src/29.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        cout << "Num1 is greater than Num2." << endl;
+    } else {
+        cout << "Num2 is greater than Num1." << endl;
+    }
+
+    return 0;
+}
